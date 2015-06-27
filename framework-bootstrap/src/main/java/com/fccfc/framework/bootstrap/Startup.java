0acbac976f6b1d37eda636b941e1b99f3eaf3908
wang wei <ww20081120@126.com>
2015-06-27 22:48:04 +0800

    Signed-off-by: wang wei <ww20081120@126.com>

---
+++ framework-bootstrap/src/main/java/com/fccfc/framework/bootstrap/Startup.java
@@ -0,0 +1,115 @@
+/**************************************************************************************** 
+ Copyright © 2003-2012 ZTEsoft Corporation. All rights reserved. Reproduction or       <br>
+ transmission in whole or in part, in any form or by any means, electronic, mechanical <br>
+ or otherwise, is prohibited without the prior written consent of the copyright owner. <br>
+ ****************************************************************************************/
+package com.fccfc.framework.bootstrap;
+
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.fccfc.framework.bootstrap.utils.cmd.Option;
+import com.fccfc.framework.bootstrap.utils.cmd.OptionParser;
+import com.fccfc.framework.cache.core.CacheConstant;
+import com.fccfc.framework.common.GlobalConstants;
+import com.fccfc.framework.common.utils.logger.Logger;
+import com.fccfc.framework.config.core.Configuration;
+
+/**
+ * <Description> <br>
+ * 
+ * @author 王伟 <br>
+ * @version 1.0 <br>
+ * @CreateDate 2015年6月23日 <br>
+ * @see com.fccfc.framework.bootstrap <br>
+ */
+public class Startup {
+
+    @Option(name = "-x", value = "classpath:/META-INF/spring/*.xml")
+    private String xmlPath;
+
+    @Option(name = "-h", value = "127.0.0.1")
+    private String host;
+
+    @Option(name = "-p", value = "7777")
+    private String port;
+
+    private static Logger logger = new Logger(Startup.class);
+
+    private static ApplicationContext context;
+
+    private static Map<String, String> paramMap = null;
+
+    /**
+     * Description: <br>
+     * 
+     * @author 王伟 <br>
+     * @param args <br>
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+
+        paramMap = OptionParser.parse(Startup.class, args);
+
+        logger.info("====================>准备加载Spring配置文件<====================");
+        ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext(StringUtils.split(paramMap
+            .get("xmlPath")));
+        context = ac;
+        ac.start();
+        logger.info("====================>Spring配置文件加载完毕<====================");
+        System.out.println(new StringBuilder().append("\n***************************************").append('\n')
+            .append("*         ").append(ManagementFactory.getRuntimeMXBean().getName()).append("        *")
+            .append('\n').append("*            ").append(Configuration.get(CacheConstant.LOCAL_MODULE_CODE))
+            .append("模块启动成功！").append("                                   *").append('\n').append("*    ")
+            .append("关闭地址为:UDP://").append(paramMap.get("host")).append(':').append(paramMap.get("port"))
+            .append("      *").append('\n').append("***************************************"));
+
+        startShutdownServer();
+
+        ac.close();
+        logger.info("====================>系统正常停止运行<====================");
+    }
+
+    private static void startShutdownServer() {
+        DatagramSocket server = null;
+        try {
+            server = new DatagramSocket(new InetSocketAddress(paramMap.get("host"), Integer.valueOf(paramMap
+                .get("port"))));
+            boolean flag = true;
+
+            byte[] buffer = new byte[1024];
+
+            while (flag) {
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                server.receive(packet);
+
+                String data = new String(packet.getData(), 0, packet.getLength(), GlobalConstants.DEFAULT_CHARSET);
+                if (StringUtils.equals(Configuration.getString(CacheConstant.LOCAL_MODULE_CODE), data)) {
+                    flag = false;
+                    System.out.println("接收到关闭服务端请求");
+                }
+
+            }
+        }
+        catch (Exception e) {
+            logger.error("服务器意外关闭", e);
+        }
+        finally {
+            if (server != null) {
+                server.close();
+            }
+        }
+    }
+
+    public static ApplicationContext getContext() {
+        return context;
+    }
+}
