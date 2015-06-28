3a22e20d75ddaeac2d68a6935ddf09f303882df4
wang wei <ww20081120@126.com>
2015-06-28 13:44:36 +0800

    Signed-off-by: wang wei <ww20081120@126.com>

---
+++ framework-message/framework-message-api/src/main/java/com/fccfc/framework/message/api/MessageTypeDef.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.fccfc.framework.message.api;
+
+/**
+ * <Description> <br>
+ * 
+ * @author wangwei<br>
+ * @version 1.0<br>
+ * @CreateDate 2015年6月28日 <br>
+ * @see com.fccfc.framework.message.api <br>
+ */
+public interface MessageTypeDef {
+
+    String TEXT = "T";
+
+    String VOICE = "V";
+
+    String MEDIA = "M";
+
+    String PICTURE = "P";
+}
