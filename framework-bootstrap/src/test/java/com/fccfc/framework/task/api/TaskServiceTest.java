a1da8759c1aea537e06f15da92bd47527a561ab2
shao.dinghui <670960993@qq.com>
2015-07-03 16:45:11 +0800

    提交Task代码测试类

---
+++ framework-bootstrap/src/test/java/com/fccfc/framework/task/api/TaskServiceTest.java
@@ -0,0 +1,41 @@
+package com.fccfc.framework.task.api;
+
+import javax.annotation.Resource;
+
+import org.apache.thrift.TException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * <Description> <br> 
+ *  
+ * @author shao.dinghui<br>
+ * @version 1.0<br>
+ * @taskId <br>
+ * @CreateDate 2015年7月3日 <br>
+ * @since V7.3<br>
+ * @see com.fccfc.framework.task.api <br>
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({
+    "classpath:/META-INF/spring/*.xml"
+})
+public class TaskServiceTest {
+
+    @Resource
+    private TaskService.Iface iface;
+    
+    /**
+     * Description: <br> 
+     *  
+     * @author shao.dinghui<br>
+     * @taskId <br>
+     * @throws TException <br>
+     */
+    @Test
+    public void testScheduleAllTask() throws TException {
+        iface.scheduleAllTask();
+    }
+}
