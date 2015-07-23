e37535d530ba973525b6aad41e24c078fd02597a
wang wei <ww20081120@126.com>
2015-07-23 15:04:24 +0800

    添加ssl的http请求

--- framework-common/src/main/java/com/fccfc/framework/common/utils/io/HttpClientUtil.java
+++ framework-common/src/main/java/com/fccfc/framework/common/utils/io/HttpClientUtil.java
@@ -8,6 +8,8 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
+import javax.net.ssl.SSLContext;
+
 import org.apache.commons.httpclient.DefaultHttpMethodRetryHandler;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpStatus;
@@ -17,8 +19,9 @@ import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.commons.httpclient.params.HttpMethodParams;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
 import com.alibaba.fastjson.JSONObject;
@@ -74,27 +77,35 @@ public final class HttpClientUtil {
         return body;
     }
 
-    /**
-     * Description: post<br>
-     * 
-     * @author 王伟<br>
-     * @taskId <br>
-     * @param url url
-     * @param json json
-     * @return static
-     * @throws UtilException <br>
-     */
     public static String post(String url, JSONObject json) throws UtilException {
-        DefaultHttpClient httpClient = new DefaultHttpClient();
+        return post(null, url, "application/json", json.toJSONString());
+    }
+
+    @SuppressWarnings({
+        "deprecation"
+    })
+    public static String post(SSLContext sslContext, String url, String contentType, String content)
+        throws UtilException {
+        org.apache.http.client.HttpClient httpClient = null;
+        if (sslContext != null) {
+            SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslContext, new String[] {
+                "TLSv1"
+            }, null, SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+            httpClient = HttpClients.custom().setSSLSocketFactory(sslsf).build();
+        }
+        else {
+            httpClient = HttpClients.custom().build();
+        }
+
         HttpPost request = new HttpPost(url);
         try {
-            StringEntity params = new StringEntity(json.toJSONString(), GlobalConstants.DEFAULT_CHARSET);
-            request.addHeader("content-type", "application/json");
+            StringEntity params = new StringEntity(content, GlobalConstants.DEFAULT_CHARSET);
+            request.addHeader("content-type", contentType);
             request.setEntity(params);
             HttpResponse response = httpClient.execute(request);
             int statusCode = response.getStatusLine().getStatusCode();
             if (statusCode == HttpStatus.SC_OK) {
-                return EntityUtils.toString(response.getEntity());
+                return EntityUtils.toString(response.getEntity(), GlobalConstants.DEFAULT_CHARSET);
             }
             else {
                 throw new UtilException(ErrorCodeDef.HTTP_REQUEST_ERROR_10035, "Http request failed, message :${0}",
