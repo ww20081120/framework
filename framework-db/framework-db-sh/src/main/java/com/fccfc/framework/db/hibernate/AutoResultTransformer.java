557cd646cb880a1725c7c9bc4f23a1056a46e529
xueyuyang <jhcy55@126.com>
2015-07-30 11:23:03 +0800

    去掉没用的方法

--- framework-db/framework-db-sh/src/main/java/com/fccfc/framework/db/hibernate/AutoResultTransformer.java
+++ framework-db/framework-db-sh/src/main/java/com/fccfc/framework/db/hibernate/AutoResultTransformer.java
@@ -173,31 +173,5 @@ public class AutoResultTransformer implements ResultTransformer {
     public List transformList(List collection) {
         return collection;
     }
-    
-    public String ClobToString(CLOB clob) throws SQLException, IOException {
-
-        String reString = "";
-
-        Reader is = clob.getCharacterStream();// 得到流
-
-        BufferedReader br = new BufferedReader(is);
-
-        String s = br.readLine();
-
-        StringBuffer sb = new StringBuffer();
-
-        while (s != null) {// 执行循环将字符串全部取出付值给StringBuffer由StringBuffer转成STRING
-
-        sb.append(s);
-
-        s = br.readLine();
-
-        }
-
-        reString = sb.toString();
-
-        return reString;
-
-    }
 
 }
