e72dcce7b88bbce84d8c8131058b5f70a7bbd628
wang wei <ww20081120@126.com>
2016-01-27 16:40:20 +0800

    Signed-off-by: wang wei <ww20081120@126.com>

--- framework-config/framework-config-core/src/main/java/com/fccfc/framework/config/core/bean/DictionaryDataPojo.java
+++ framework-config/framework-config-core/src/main/java/com/fccfc/framework/config/core/bean/DictionaryDataPojo.java
@@ -7,6 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import com.fccfc.framework.db.core.BaseEntity;
+
 /**
  * <Description> <br>
  * 
@@ -23,71 +24,89 @@ public class DictionaryDataPojo extends BaseEntity {
      * serialVersionUID
      */
     private static final long serialVersionUID = 1L;
+
     /**
      * DICT_DATA_ID
      */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "DICT_DATA_ID")
-    private Long dictDataId;
+    private Integer dictDataId;
+
     /**
      * DICT_CODE
      */
     @Column(name = "DICT_CODE")
     private String dictCode;
+
     /**
      * DICT_DATA_NAME
      */
     @Column(name = "DICT_DATA_NAME")
     private String dictDataName;
+
     /**
      * DICT_DATA_VALUE
      */
     @Column(name = "DICT_DATA_VALUE")
     private String dictDataValue;
+
     /**
      * IS_FIXED
      */
     @Column(name = "IS_FIXED")
     private String isFixed;
+
     /**
      * IS_CANCEL
      */
     @Column(name = "IS_CANCEL")
     private String isCancel;
-    public Long getDictDataId() {
+
+    public Integer getDictDataId() {
         return dictDataId;
     }
-    public void setDictDataId(Long dictDataId) {
+
+    public void setDictDataId(Integer dictDataId) {
         this.dictDataId = dictDataId;
     }
+
     public String getDictCode() {
         return dictCode;
     }
+
     public void setDictCode(String dictCode) {
         this.dictCode = dictCode;
     }
+
     public String getDictDataName() {
         return dictDataName;
     }
+
     public void setDictDataName(String dictDataName) {
         this.dictDataName = dictDataName;
     }
+
     public String getDictDataValue() {
         return dictDataValue;
     }
+
     public void setDictDataValue(String dictDataValue) {
         this.dictDataValue = dictDataValue;
     }
+
     public String getIsFixed() {
         return isFixed;
     }
+
     public void setIsFixed(String isFixed) {
         this.isFixed = isFixed;
     }
+
     public String getIsCancel() {
         return isCancel;
     }
+
     public void setIsCancel(String isCancel) {
         this.isCancel = isCancel;
     }
