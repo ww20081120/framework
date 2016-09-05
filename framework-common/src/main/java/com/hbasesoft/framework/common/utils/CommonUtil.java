1253691a24034a818128b2cf6e18cbc0da2dfe64
hejiawen <18151603769@163.com>
2016-09-05 09:37:47 +0800

    Signed-off-by: hejiawen <18151603769@163.com>

--- framework-common/src/main/java/com/hbasesoft/framework/common/utils/CommonUtil.java
+++ framework-common/src/main/java/com/hbasesoft/framework/common/utils/CommonUtil.java
@@ -37,22 +37,24 @@ public final class CommonUtil {
      * Description: 字符串是为NULL或为空<br>
      * 
      * @author 王伟 <br>
-     * @param str <br>
+     * @param str
+     *            <br>
      * @return <br>
      */
     public static boolean isEmpty(String str) {
-        return StringUtils.isEmpty(str);
+	return StringUtils.isEmpty(str);
     }
 
     /**
      * Description:字符串不为NULL也不为空 <br>
      * 
      * @author 王伟 <br>
-     * @param str <br>
+     * @param str
+     *            <br>
      * @return <br>
      */
     public static boolean isNotEmpty(String str) {
-        return !isEmpty(str);
+	return !isEmpty(str);
     }
 
     /**
@@ -60,107 +62,119 @@ public final class CommonUtil {
      * 
      * @author yang.zhipeng <br>
      * @taskId <br>
-     * @param t <br>
-     * @param <T> <br>
+     * @param t
+     *            <br>
+     * @param <T>
+     *            <br>
      * @return <br>
      */
     public static <T> boolean isEmpty(T[] t) {
-        return t == null || t.length == 0;
+	return t == null || t.length == 0;
     }
 
     /**
      * 判断数组不为NULL也不为空
      * 
-     * @param t <br>
-     * @param <T> <br>
+     * @param t
+     *            <br>
+     * @param <T>
+     *            <br>
      * @return <br>
      */
     public static <T> boolean isNotEmpty(T[] t) {
-        return !isEmpty(t);
+	return !isEmpty(t);
     }
 
     /**
      * Description: 集合是否为NULL或为空<br>
      * 
      * @author 王伟 <br>
-     * @param col <br>
+     * @param col
+     *            <br>
      * @return <br>
      */
     public static boolean isEmpty(Collection<?> col) {
-        return col == null || col.isEmpty();
+	return col == null || col.isEmpty();
     }
 
     /**
      * Description:集合不为NULL也不为空 <br>
      * 
      * @author 王伟 <br>
-     * @param col <br>
+     * @param col
+     *            <br>
      * @return <br>
      */
     public static boolean isNotEmpty(Collection<?> col) {
-        return !isEmpty(col);
+	return !isEmpty(col);
     }
 
     /**
      * Description: map是否为NULL或为空<br>
      * 
      * @author 王伟 <br>
-     * @param map <br>
+     * @param map
+     *            <br>
      * @return <br>
      */
     public static boolean isEmpty(Map<?, ?> map) {
-        return map == null || map.isEmpty();
+	return map == null || map.isEmpty();
     }
 
     /**
      * Description:map不为NULL也不为空 <br>
      * 
      * @author 王伟 <br>
-     * @param map <br>
+     * @param map
+     *            <br>
      * @return <br>
      */
     public static boolean isNotEmpty(Map<?, ?> map) {
-        return !isEmpty(map);
+	return !isEmpty(map);
     }
 
     /**
      * Description: <br>
      * 
      * @author 王伟 <br>
-     * @param str <br>
+     * @param str
+     *            <br>
      * @return <br>
      */
     public static String lowerCaseFirstChar(String str) {
-        if (isNotEmpty(str)) {
-            char firstChar = str.charAt(0);
-            if (Character.isUpperCase(firstChar)) {
-                StringBuilder sb = new StringBuilder(str);
-                sb.setCharAt(0, Character.toLowerCase(firstChar));
-                str = sb.toString();
-            }
-        }
-        return str;
+	if (isNotEmpty(str)) {
+	    char firstChar = str.charAt(0);
+	    if (Character.isUpperCase(firstChar)) {
+		StringBuilder sb = new StringBuilder(str);
+		sb.setCharAt(0, Character.toLowerCase(firstChar));
+		str = sb.toString();
+	    }
+	}
+	return str;
     }
 
     /**
      * 消息格式化
      * 
-     * @param message message <br>
-     * @param params params <br>
+     * @param message
+     *            message <br>
+     * @param params
+     *            params <br>
      * @return String <br>
      */
     public static String messageFormat(String message, Object... params) {
-        return isNotEmpty(params) ? MessageFormat.format(message, params) : message;
+	return isNotEmpty(params) ? MessageFormat.format(message, params) : message;
     }
 
     /**
      * 判断是否是空对象
      * 
-     * @param obj obj <br>
+     * @param obj
+     *            obj <br>
      * @return boolean <br>
      */
     public static boolean isNull(Object obj) {
-        return null == obj;
+	return null == obj;
     }
 
     /**
@@ -169,22 +183,23 @@ public final class CommonUtil {
      * @return 事务ID <br>
      */
     public static String getTransactionID() {
-        return UUID.randomUUID().toString().replace("-", GlobalConstants.BLANK);
+	return UUID.randomUUID().toString().replace("-", GlobalConstants.BLANK);
     }
 
     /**
      * Description: 获取指定位数的随机数<br>
      * 
      * @author 王伟 <br>
-     * @param size <br>
+     * @param size
+     *            <br>
      * @return <br>
      */
     public static String getRandomNumber(int size) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < size; i++) {
-            sb.append((char) ('0' + RANDOM.nextInt(10)));
-        }
-        return sb.toString();
+	StringBuilder sb = new StringBuilder();
+	for (int i = 0; i < size; i++) {
+	    sb.append((char) ('0' + RANDOM.nextInt(10)));
+	}
+	return sb.toString();
     }
 
     /**
@@ -192,27 +207,28 @@ public final class CommonUtil {
      * 
      * @author yang.zhipeng <br>
      * @taskId <br>
-     * @param size <br>
+     * @param size
+     *            <br>
      * @return <br>
      */
     public static String getRandomChar(int size) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < size; i++) {
-            switch (RANDOM.nextInt(10) % 3) {
-                case 0:
-                    sb.append((char) ('0' + RANDOM.nextInt(10)));
-                    break;
-                case 1:
-                    sb.append((char) ('a' + RANDOM.nextInt(26)));
-                    break;
-                case 2:
-                    sb.append((char) ('A' + RANDOM.nextInt(26)));
-                    break;
-                default:
-                    ;
-            }
-        }
-        return sb.toString();
+	StringBuilder sb = new StringBuilder();
+	for (int i = 0; i < size; i++) {
+	    switch (RANDOM.nextInt(10) % 3) {
+	    case 0:
+		sb.append((char) ('0' + RANDOM.nextInt(10)));
+		break;
+	    case 1:
+		sb.append((char) ('a' + RANDOM.nextInt(26)));
+		break;
+	    case 2:
+		sb.append((char) ('A' + RANDOM.nextInt(26)));
+		break;
+	    default:
+		;
+	    }
+	}
+	return sb.toString();
     }
 
     /**
@@ -220,29 +236,31 @@ public final class CommonUtil {
      * 
      * @author yang.zhipeng <br>
      * @taskId <br>
-     * @param obj <br>
+     * @param obj
+     *            <br>
      * @return <br>
      */
     public static String getString(Object obj) {
-        String result = null;
-        if (obj != null) {
-            result = obj instanceof String ? (String) obj : obj.toString();
-        }
-        return result;
+	String result = null;
+	if (obj != null) {
+	    result = obj instanceof String ? (String) obj : obj.toString();
+	}
+	return result;
     }
 
     /**
      * Description:getDate <br>
      * 
      * @author 王伟 <br>
-     * @param time <br
+     * @param time
+     *            <br
      * @return <br>
      */
     public static Date getDate(Long time) {
-        if (time != null) {
-            return new Date(time);
-        }
-        return null;
+	if (time != null) {
+	    return new Date(time);
+	}
+	return null;
     }
 
     /**
@@ -250,36 +268,35 @@ public final class CommonUtil {
      * 
      * @author yang.zhipeng <br>
      * @taskId <br>
-     * @param s <br>
+     * @param s
+     *            <br>
      * @return <br>
-     * @throws UtilException <br>
+     * @throws UtilException
+     *             <br>
      */
     public static final String md5(String s) throws UtilException {
-        char hexDigits[] = {
-            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
-        };
-        try {
-            byte[] btInput = s.getBytes();
-            // 获得MD5摘要算法的 MessageDigest 对象
-            MessageDigest mdInst = MessageDigest.getInstance("MD5");
-            // 使用指定的字节更新摘要
-            mdInst.update(btInput);
-            // 获得密文
-            byte[] md = mdInst.digest();
-            // 把密文转换成十六进制的字符串形式
-            int j = md.length;
-            char str[] = new char[j * 2];
-            int k = 0;
-            for (int i = 0; i < j; i++) {
-                byte byte0 = md[i];
-                str[k++] = hexDigits[byte0 >>> 4 & 0xf];
-                str[k++] = hexDigits[byte0 & 0xf];
-            }
-            return new String(str);
-        }
-        catch (Exception e) {
-            throw new UtilException(ErrorCodeDef.SYSTEM_ERROR_10001);
-        }
+	char hexDigits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+	try {
+	    byte[] btInput = s.getBytes();
+	    // 获得MD5摘要算法的 MessageDigest 对象
+	    MessageDigest mdInst = MessageDigest.getInstance("MD5");
+	    // 使用指定的字节更新摘要
+	    mdInst.update(btInput);
+	    // 获得密文
+	    byte[] md = mdInst.digest();
+	    // 把密文转换成十六进制的字符串形式
+	    int j = md.length;
+	    char str[] = new char[j * 2];
+	    int k = 0;
+	    for (int i = 0; i < j; i++) {
+		byte byte0 = md[i];
+		str[k++] = hexDigits[byte0 >>> 4 & 0xf];
+		str[k++] = hexDigits[byte0 & 0xf];
+	    }
+	    return new String(str);
+	} catch (Exception e) {
+	    throw new UtilException(ErrorCodeDef.SYSTEM_ERROR_10001);
+	}
     }
 
     /**
@@ -292,27 +309,27 @@ public final class CommonUtil {
      * @return <br>
      */
     public static final Integer[] splitId(String idStr, String splitor) {
-        Integer[] ids = null;
-        if (CommonUtil.isNotEmpty(idStr)) {
-            String[] strs = StringUtils.split(idStr, splitor);
-            ids = new Integer[strs.length];
-            for (int i = 0; i < strs.length; i++) {
-                ids[i] = Integer.valueOf(strs[i]);
-            }
-        }
-        return ids;
+	Integer[] ids = null;
+	if (CommonUtil.isNotEmpty(idStr)) {
+	    String[] strs = StringUtils.split(idStr, splitor);
+	    ids = new Integer[strs.length];
+	    for (int i = 0; i < strs.length; i++) {
+		ids[i] = Integer.valueOf(strs[i]);
+	    }
+	}
+	return ids;
     }
 
     public static final Long[] splitIdsByLong(String idStr, String splitor) {
-        Long[] ids = null;
-        if (CommonUtil.isNotEmpty(idStr)) {
-            String[] strs = StringUtils.split(idStr, splitor);
-            ids = new Long[strs.length];
-            for (int i = 0; i < strs.length; i++) {
-                ids[i] = Long.valueOf(strs[i]);
-            }
-        }
-        return ids;
+	Long[] ids = null;
+	if (CommonUtil.isNotEmpty(idStr)) {
+	    String[] strs = StringUtils.split(idStr, splitor);
+	    ids = new Long[strs.length];
+	    for (int i = 0; i < strs.length; i++) {
+		ids[i] = Long.valueOf(strs[i]);
+	    }
+	}
+	return ids;
     }
 
     /**
@@ -324,7 +341,7 @@ public final class CommonUtil {
      * @return <br>
      */
     public static final Integer[] splitId(String idStr) {
-        return splitId(idStr, GlobalConstants.SPLITOR);
+	return splitId(idStr, GlobalConstants.SPLITOR);
     }
 
     /**
@@ -332,18 +349,54 @@ public final class CommonUtil {
      * 
      * @author 王伟<br>
      * @taskId <br>
-     * @param key <br>
-     * @param matchValue <br>
+     * @param key
+     *            <br>
+     * @param matchValue
+     *            <br>
      * @return <br>
      */
     public static boolean match(String key, String matchValue) {
-        boolean ismatch = false;
-        String value = getString(key);
-        if (CommonUtil.isNotEmpty(value) && CommonUtil.isNotEmpty(matchValue)) {
-            ismatch = new StringBuilder().append(GlobalConstants.SPLITOR).append(value).append(GlobalConstants.SPLITOR)
-                .indexOf(new StringBuilder().append(GlobalConstants.SPLITOR).append(matchValue)
-                    .append(GlobalConstants.SPLITOR).toString()) != -1;
-        }
-        return ismatch;
+	boolean ismatch = false;
+	String value = getString(key);
+	if (CommonUtil.isNotEmpty(value) && CommonUtil.isNotEmpty(matchValue)) {
+	    ismatch = new StringBuilder().append(GlobalConstants.SPLITOR).append(value).append(GlobalConstants.SPLITOR)
+		    .indexOf(new StringBuilder().append(GlobalConstants.SPLITOR).append(matchValue)
+			    .append(GlobalConstants.SPLITOR).toString()) != -1;
+	}
+	return ismatch;
+    }
+
+    /**
+     * 
+     * Description: 除去字符串中的所有符号<br>
+     * 
+     * @author 何佳文<br>
+     * @taskId <br>
+     * @param str
+     * @return <br>
+     */
+    public static String removeAllSymbol(String str) {
+	if (isNotEmpty(str)) {
+	    return str.replaceAll("[\\pP\\p{Punct}]", "");
+	}
+	return "";
+    }
+
+    /**
+     * 
+     * Description: 除去字符串中symbol意外的所有符号<br>
+     * 
+     * @author 何佳文<br>
+     * @taskId <br>
+     * @param str
+     * @param symbol
+     * @return <br>
+     */
+    public static String removeAllSymbol(String str, String symbol) {
+	if (isNotEmpty(str)) {
+	    String random = getRandomChar(10);
+	    return str.replace(symbol, random).replaceAll("[\\pP\\p{Punct}]", "").replace(random, symbol);
+	}
+	return "";
     }
 }
