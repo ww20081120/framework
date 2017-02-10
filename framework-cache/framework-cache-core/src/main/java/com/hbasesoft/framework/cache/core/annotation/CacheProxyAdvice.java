ddc7a700d59a1d5b5873402761892c72d454c7c9
wang wei <ww20081120@126.com>
2017-02-10 17:56:05 +0800

    修改加载顺序

--- framework-cache/framework-cache-core/src/main/java/com/hbasesoft/framework/cache/core/annotation/CacheProxyAdvice.java
+++ framework-cache/framework-cache-core/src/main/java/com/hbasesoft/framework/cache/core/annotation/CacheProxyAdvice.java
@@ -1,21 +1,21 @@
 package com.hbasesoft.framework.cache.core.annotation;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.lang.reflect.Proxy;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
 
 import com.hbasesoft.framework.cache.core.handler.CachePorxyInvocationHandler;
 import com.hbasesoft.framework.common.ErrorCodeDef;
 import com.hbasesoft.framework.common.utils.Assert;
 import com.hbasesoft.framework.common.utils.CommonUtil;
-import com.hbasesoft.framework.common.utils.ContextHolder;
-import com.hbasesoft.framework.common.utils.logger.Logger;
 import com.hbasesoft.framework.common.utils.logger.LoggerUtil;
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.config.BeanPostProcessor;
-import org.springframework.stereotype.Component;
-
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
-import java.lang.reflect.Proxy;
 
 /**
  * <Description> <br>
@@ -28,8 +28,9 @@ import java.lang.reflect.Proxy;
  * @since V1.0<br>
  */
 @Component
-public class CacheProxyAdvice implements BeanPostProcessor {
+public class CacheProxyAdvice implements BeanPostProcessor, ApplicationContextAware {
 
+    private ApplicationContext applicationContext;
 
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
@@ -38,20 +39,20 @@ public class CacheProxyAdvice implements BeanPostProcessor {
 
         for (Method method : methods) {
             String name = method.getName();
-            if (name.length() > 3 && name.startsWith("set")
-                    && method.getParameterTypes().length == 1
-                    && Modifier.isPublic(method.getModifiers())
-                    && !Modifier.isStatic(method.getModifiers())) {
+            if (name.length() > 3 && name.startsWith("set") && method.getParameterTypes().length == 1
+                && Modifier.isPublic(method.getModifiers()) && !Modifier.isStatic(method.getModifiers())) {
                 try {
                     CacheProxy cacheProxy = method.getAnnotation(CacheProxy.class);
                     if (cacheProxy != null) {
                         Object value = refer(cacheProxy, method.getParameterTypes()[0]);
                         if (value != null) {
-                            method.invoke(bean, new Object[]{});
+                            method.invoke(bean, new Object[] {});
                         }
                     }
-                } catch (Throwable e) {
-                    LoggerUtil.error("Failed to init remote service reference at method " + name + " in class " + bean.getClass().getName() + ", cause: " + e.getMessage(), e);
+                }
+                catch (Throwable e) {
+                    LoggerUtil.error("Failed to init proxy cache at method " + name + " in class "
+                        + bean.getClass().getName() + ", cause: " + e.getMessage(), e);
                 }
             }
         }
@@ -70,40 +71,54 @@ public class CacheProxyAdvice implements BeanPostProcessor {
                         field.set(bean, value);
                     }
                 }
-            } catch (Throwable e) {
-                LoggerUtil.error("Failed to init remote service reference at filed " + field.getName() + " in class " + bean.getClass().getName() + ", cause: " + e.getMessage(), e);
+            }
+            catch (Throwable e) {
+                LoggerUtil.error("Failed to init proxy cache at filed " + field.getName() + " in class "
+                    + bean.getClass().getName() + ", cause: " + e.getMessage(), e);
             }
         }
         return bean;
     }
-
 
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
         return bean;
     }
 
+    @SuppressWarnings("unchecked")
     private <T> T refer(CacheProxy cacheProxy, Class<T> clazz) {
 
         if (Modifier.isAbstract(clazz.getModifiers())) {
             T target = null;
             if (CommonUtil.isNotEmpty(cacheProxy.name())) {
-                target = ContextHolder.getContext().getBean(cacheProxy.name(), clazz);
-            } else {
-                target = ContextHolder.getContext().getBean(clazz);
+                target = applicationContext.getBean(cacheProxy.name(), clazz);
+            }
+            else {
+                target = applicationContext.getBean(clazz);
             }
 
             Assert.notNull(target, ErrorCodeDef.PROXY_TARGET_NOT_FOUND, clazz);
 
-
             CachePorxyInvocationHandler invocationHandler = new CachePorxyInvocationHandler(target, cacheProxy, clazz);
 
-            T proxyObj =  (T) Proxy.newProxyInstance(clazz.getClassLoader(),
-                    clazz.getInterfaces(), invocationHandler);
+            T proxyObj = (T) Proxy.newProxyInstance(clazz.getClassLoader(), clazz.getInterfaces(), invocationHandler);
 
             LoggerUtil.info("Success cache proxy clazz[{0}].", clazz);
             return proxyObj;
         }
         return null;
+    }
+
+    /**
+     * Description: <br>
+     * 
+     * @author 王伟<br>
+     * @taskId <br>
+     * @param applicationContext
+     * @throws BeansException <br>
+     */
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
     }
 }
