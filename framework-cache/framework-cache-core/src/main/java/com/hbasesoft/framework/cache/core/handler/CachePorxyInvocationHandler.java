d9fa114e530284c75f21ff5b17f47353487040bc
wang wei <ww20081120@126.com>
2017-03-16 11:19:23 +0800

    修改cacheable为false时 代码不执行的问题

--- framework-cache/framework-cache-core/src/main/java/com/hbasesoft/framework/cache/core/handler/CachePorxyInvocationHandler.java
+++ framework-cache/framework-cache-core/src/main/java/com/hbasesoft/framework/cache/core/handler/CachePorxyInvocationHandler.java
@@ -57,7 +57,8 @@ public class CachePorxyInvocationHandler implements InvocationHandler {
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         Object result = null;
         CacheMethodConfig config = cacheMethodConfigMap.get(method.getName());
-        if (config == null || Void.class.equals(method.getAnnotatedReturnType().getType().getTypeName())) {
+        if (config == null || config.cacheAble()
+            || Void.class.equals(method.getAnnotatedReturnType().getType().getTypeName())) {
             result = method.invoke(target, args);
         }
         else {
