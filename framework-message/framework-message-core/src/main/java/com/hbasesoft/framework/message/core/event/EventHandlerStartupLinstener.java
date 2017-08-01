c4e10a90223bc19e01d14d917955a8f29c9db66a
wang wei <ww20081120@126.com>
2017-08-01 22:13:16 +0800

    修复消耗cpu的bug

--- framework-message/framework-message-core/src/main/java/com/hbasesoft/framework/message/core/event/EventHandlerStartupLinstener.java
+++ framework-message/framework-message-core/src/main/java/com/hbasesoft/framework/message/core/event/EventHandlerStartupLinstener.java
@@ -91,26 +91,33 @@ public class EventHandlerStartupLinstener extends StartupListenerAdapter {
             lisenerExecutor.execute(new Runnable() {
                 @Override
                 public void run() {
-                    while (flag) {
-                        for (Entry<String, EventLinsener> entry : eventLinsenerHolder.entrySet()) {
-                            String event = entry.getKey();
-                            try {
-                                byte[] data = queue.pop(3, event);
-                                if (data != null) {
-                                    LoggerUtil.info("receive message by thread[{0}]", Thread.currentThread().getId());
-                                    arrayBlockingQueue.put(new EventConsummer(entry.getValue(), data, event));
+                    try {
+                        while (flag) {
+                            if (CommonUtil.isNotEmpty(eventLinsenerHolder)) {
+                                for (Entry<String, EventLinsener> entry : eventLinsenerHolder.entrySet()) {
+                                    String event = entry.getKey();
+                                    try {
+                                        byte[] data = queue.pop(3, event);
+                                        if (data != null) {
+                                            LoggerUtil.info("receive message by thread[{0}]",
+                                                Thread.currentThread().getId());
+                                            arrayBlockingQueue.put(new EventConsummer(entry.getValue(), data, event));
+                                        }
+                                    }
+                                    catch (Exception e) {
+                                        LoggerUtil.error(e);
+                                        Thread.sleep(1000);
+
+                                    }
                                 }
                             }
-                            catch (Exception e) {
-                                LoggerUtil.error(e);
-                                try {
-                                    Thread.sleep(1000);
-                                }
-                                catch (InterruptedException e1) {
-                                    LoggerUtil.error(e1);
-                                }
+                            else {
+                                Thread.sleep(1000);
                             }
                         }
+                    }
+                    catch (InterruptedException e) {
+                        LoggerUtil.error(e);
                     }
                 }
             });
