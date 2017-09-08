3e4389288df55352c49166b06cf7e306712d6d86
wang wei <ww20081120@126.com>
2017-09-08 17:50:30 +0800

    1

--- framework-workflow/framework-workflow-plugin-rule/src/main/java/com/hbasesoft/framework/workflow/plugin/rule/ToolsInterceptor.java
+++ framework-workflow/framework-workflow-plugin-rule/src/main/java/com/hbasesoft/framework/workflow/plugin/rule/ToolsInterceptor.java
@@ -5,30 +5,42 @@
  ****************************************************************************************/
 package com.hbasesoft.framework.workflow.plugin.rule;
 
-import java.util.Map.Entry;
+import java.util.Map;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Component;
 
+import com.hbasesoft.framework.common.GlobalConstants;
 import com.hbasesoft.framework.common.utils.CommonUtil;
+import com.hbasesoft.framework.common.utils.ContextHolder;
 import com.hbasesoft.framework.workflow.core.FlowBean;
 import com.hbasesoft.framework.workflow.core.FlowComponentInterceptor;
 import com.hbasesoft.framework.workflow.core.FlowContext;
 
-import groovy.lang.Binding;
-import groovy.lang.GroovyShell;
-
 /**
- * <Description> 条件组合<br>
+ * <Description> <br>
  * 
  * @author 王伟<br>
  * @version 1.0<br>
  * @taskId <br>
- * @CreateDate 2017年9月4日 <br>
+ * @CreateDate 2017年9月6日 <br>
  * @since V1.0<br>
- * @see com.hbasesoft.framework.workflow.plugin.rule <br>
+ * @see com.hbasesoft.framework.workflow.plugin.tools <br>
  */
 @Component
-public class ConditionInterceptor implements FlowComponentInterceptor {
+public class ToolsInterceptor implements FlowComponentInterceptor {
+
+    /**
+     * Description: <br>
+     * 
+     * @author 王伟<br>
+     * @taskId <br>
+     * @return <br>
+     */
+    @Override
+    public int order() {
+        return -1;
+    }
 
     /**
      * Description: <br>
@@ -41,18 +53,18 @@ public class ConditionInterceptor implements FlowComponentInterceptor {
      */
     @Override
     public boolean before(FlowBean flowBean, FlowContext flowContext) {
-        String condition = (String) flowContext.getFlowConfig().getConfigAttrMap().get("condition");
-        if (CommonUtil.isNotEmpty(condition)) {
-            Binding binding = new Binding(flowContext.getParamMap());
-            if (CommonUtil.isNotEmpty(flowContext.getExtendUtils())) {
-                for (Entry<String, Object> util : flowContext.getExtendUtils().entrySet()) {
-                    binding.setProperty(util.getKey(), util.getValue());
+        String tools = (String) flowContext.getFlowConfig().getConfigAttrMap().get("tools");
+        if (CommonUtil.isNotEmpty(tools)) {
+            String[] ts = StringUtils.split(tools, GlobalConstants.SPLITOR);
+            if (CommonUtil.isNotEmpty(ts)) {
+                Map<String, Object> toolMap = flowContext.getExtendUtils();
+                for (String t : ts) {
+                    if (!toolMap.containsKey(t)) {
+                        Object tool = ContextHolder.getContext().getBean(t);
+                        toolMap.put(t, tool);
+                    }
                 }
             }
-            binding.setProperty("flowBean", flowBean);
-            GroovyShell shell = new GroovyShell(binding);
-            Object value = shell.evaluate(condition);
-            return value != null && "true".equals(value.toString().toLowerCase());
         }
         return true;
     }
