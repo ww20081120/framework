c88e2e983f8b4b26ca8f5b2d095946c9b331ff52
wang wei <ww20081120@126.com>
2018-01-29 16:06:06 +0800

    update log config

--- framework-log/framework-log-core/src/main/java/com/hbasesoft/framework/log/core/advice/BusinessTransactionAdivce.java
+++ framework-log/framework-log-core/src/main/java/com/hbasesoft/framework/log/core/advice/BusinessTransactionAdivce.java
@@ -10,7 +10,7 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 import com.hbasesoft.framework.common.FrameworkException;
 import com.hbasesoft.framework.log.core.TransLogUtil;
@@ -26,7 +26,7 @@ import com.hbasesoft.framework.log.core.TransLogUtil;
  * @see com.hbasesoft.framework.log.advice <br>
  */
 @Aspect
-@Configuration
+@Component
 public class BusinessTransactionAdivce {
 
     @Pointcut("execution(public * com.hbasesoft..*(..))")
@@ -52,43 +52,4 @@ public class BusinessTransactionAdivce {
             throw new FrameworkException(e);
         }
     }
-    //
-    // /**
-    // * 方法执行前拦截
-    // *
-    // * @param point 参数
-    // * @throws FrameworkException <br>
-    // */
-    // @Before("log()")
-    // public void before(JoinPoint point) throws FrameworkException {
-    // MethodSignature methodSignature = (MethodSignature) point.getSignature();
-    // TransLogUtil.before(point.getTarget(), methodSignature.getMethod(), point.getArgs());
-    // }
-    //
-    // /**
-    // * 方法正常执行结束拦截
-    // *
-    // * @param point 参数
-    // * @param returnValue 方法返回值
-    // * @throws ServiceException
-    // */
-    // @AfterReturning(pointcut = "log()", returning = "returnValue")
-    // public void afterReturning(JoinPoint point, Object returnValue) {
-    // MethodSignature methodSignature = (MethodSignature) point.getSignature();
-    // TransLogUtil.afterReturning(point.getTarget(), methodSignature.getMethod(), returnValue);
-    // }
-    //
-    // /**
-    // * 方法异常执行结束拦截
-    // *
-    // * @param point 参数
-    // * @param ex 异常信息
-    // * @throws ServiceException
-    // */
-    // @AfterThrowing(pointcut = "log()", throwing = "ex")
-    // public void afterThrowing(JoinPoint point, Exception ex) {
-    // MethodSignature methodSignature = (MethodSignature) point.getSignature();
-    // TransLogUtil.afterThrowing(point.getTarget(), methodSignature.getMethod(), ex);
-    // }
-
 }
