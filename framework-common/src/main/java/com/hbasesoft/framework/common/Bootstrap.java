653eb135d15de34d928e4ebcc92393bc86e821e2
wangwei <ww20081120@139.com>
2018-08-30 15:49:27 +0800

    修改接口目录

--- framework-common/src/main/java/com/hbasesoft/framework/common/Bootstrap.java
+++ framework-common/src/main/java/com/hbasesoft/framework/common/Bootstrap.java
@@ -13,9 +13,13 @@ import java.util.List;
 import java.util.ServiceLoader;
 
 import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.ApplicationContext;
 
+import com.hbasesoft.framework.common.utils.CommonUtil;
 import com.hbasesoft.framework.common.utils.PropertyHolder;
+import com.hbasesoft.framework.common.utils.io.ProtocolUtil;
+import com.hbasesoft.framework.common.utils.io.ProtocolUtil.Address;
 import com.hbasesoft.framework.common.utils.logger.Logger;
 
 /**
@@ -42,6 +46,12 @@ public class Bootstrap {
     private static List<StartupListener> listenerList = null;
 
     public static void before() {
+        // 检测并设置http\https\ftp\socks的代理
+        setHttpProxy();
+        setHttpsProxy();
+        setFtpProxy();
+        setSocksProxy();
+
         ServiceLoader<StartupListener> loader = ServiceLoader.load(StartupListener.class);
         if (loader != null) {
             listenerList = new ArrayList<StartupListener>();
@@ -86,5 +96,98 @@ public class Bootstrap {
             .append("***************************************"));
         logger.info("====================>系统正常启动<====================");
 
+    }
+
+    private static void setHttpProxy() {
+        // 检查并设置http代理
+        if (StringUtils.isEmpty(System.getProperty("http.proxyHost"))) {
+            String httpProxy = PropertyHolder.getProperty("http.proxy");
+            if (StringUtils.isEmpty(httpProxy)) {
+                httpProxy = System.getenv("http_proxy");
+            }
+
+            if (StringUtils.isNotEmpty(httpProxy)) {
+                Address[] protocols = ProtocolUtil.parseAddress(httpProxy);
+                if (CommonUtil.isNotEmpty(protocols)) {
+                    Address addr = protocols[0];
+                    System.setProperty("http.proxyHost", addr.getHost());
+                    System.setProperty("http.proxyPort", addr.getPort() + GlobalConstants.BLANK);
+                    String nonProxyHosts = PropertyHolder.getProperty("http.nonProxyHosts");
+                    if (StringUtils.isNotEmpty(nonProxyHosts)) {
+                        System.setProperty("http.nonProxyHosts", nonProxyHosts);
+                    }
+                    logger.info("set http proxy[{0}:{1}] success", addr.getHost(), addr.getPort());
+                }
+            }
+        }
+    }
+
+    private static void setHttpsProxy() {
+        // 检查并设置http代理
+        if (StringUtils.isEmpty(System.getProperty("https.proxyHost"))) {
+            String httpsProxy = PropertyHolder.getProperty("https.proxy");
+            if (StringUtils.isEmpty(httpsProxy)) {
+                httpsProxy = System.getenv("https_proxy");
+            }
+
+            if (StringUtils.isNotEmpty(httpsProxy)) {
+                Address[] protocols = ProtocolUtil.parseAddress(httpsProxy);
+                if (CommonUtil.isNotEmpty(protocols)) {
+                    Address addr = protocols[0];
+                    System.setProperty("https.proxyHost", addr.getHost());
+                    System.setProperty("https.proxyPort", addr.getPort() + GlobalConstants.BLANK);
+                    String nonProxyHosts = PropertyHolder.getProperty("https.nonProxyHosts");
+                    if (StringUtils.isNotEmpty(nonProxyHosts)) {
+                        System.setProperty("https.nonProxyHosts", nonProxyHosts);
+                    }
+                    logger.info("set https proxy[{0}:{1}] success", addr.getHost(), addr.getPort());
+                }
+            }
+        }
+    }
+
+    private static void setFtpProxy() {
+        // 检查并设置http代理
+        if (StringUtils.isEmpty(System.getProperty("ftp.proxyHost"))) {
+            String ftpProxy = PropertyHolder.getProperty("ftp.proxy");
+            if (StringUtils.isEmpty(ftpProxy)) {
+                ftpProxy = System.getenv("ftp_proxy");
+            }
+
+            if (StringUtils.isNotEmpty(ftpProxy)) {
+                Address[] protocols = ProtocolUtil.parseAddress(ftpProxy);
+                if (CommonUtil.isNotEmpty(protocols)) {
+                    Address addr = protocols[0];
+                    System.setProperty("ftp.proxyHost", addr.getHost());
+                    System.setProperty("ftp.proxyPort", addr.getPort() + GlobalConstants.BLANK);
+                    String nonProxyHosts = PropertyHolder.getProperty("ftp.nonProxyHosts");
+                    if (StringUtils.isNotEmpty(nonProxyHosts)) {
+                        System.setProperty("ftp.nonProxyHosts", nonProxyHosts);
+                    }
+                    logger.info("set ftp proxy[{0}:{1}] success", addr.getHost(), addr.getPort());
+                }
+            }
+        }
+    }
+
+    private static void setSocksProxy() {
+        // 检查并设置http代理
+        if (StringUtils.isEmpty(System.getProperty("socksProxyHost"))) {
+            String socksProxy = PropertyHolder.getProperty("socksProxy");
+            if (StringUtils.isEmpty(socksProxy)) {
+                socksProxy = System.getenv("socks_proxy");
+            }
+
+            if (StringUtils.isNotEmpty(socksProxy)) {
+                Address[] protocols = ProtocolUtil.parseAddress(socksProxy);
+                if (CommonUtil.isNotEmpty(protocols)) {
+                    Address addr = protocols[0];
+                    System.setProperty("socksProxyHost", addr.getHost());
+                    System.setProperty("socksProxyPort", addr.getPort() + GlobalConstants.BLANK);
+
+                    logger.info("set socks proxy[{0}:{1}] success", addr.getHost(), addr.getPort());
+                }
+            }
+        }
     }
 }
