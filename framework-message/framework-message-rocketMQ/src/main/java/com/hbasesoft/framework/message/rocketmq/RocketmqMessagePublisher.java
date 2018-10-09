41d641678da60ac2e942d605f98f50eafc98f859
wangwei <ww20081120@139.com>
2018-10-09 10:49:22 +0800

    修改配置文件

--- framework-message/framework-message-rocketMQ/src/main/java/com/hbasesoft/framework/message/rocketmq/RocketmqMessagePublisher.java
+++ framework-message/framework-message-rocketMQ/src/main/java/com/hbasesoft/framework/message/rocketmq/RocketmqMessagePublisher.java
@@ -6,14 +6,13 @@ import org.apache.rocketmq.common.message.Message;
 
 import com.hbasesoft.framework.common.ErrorCodeDef;
 import com.hbasesoft.framework.common.GlobalConstants;
-import com.hbasesoft.framework.common.utils.Assert;
+import com.hbasesoft.framework.common.utils.PropertyHolder;
 import com.hbasesoft.framework.common.utils.UtilException;
 import com.hbasesoft.framework.common.utils.logger.Logger;
 import com.hbasesoft.framework.message.core.MessagePublisher;
 import com.hbasesoft.framework.message.rocketmq.factory.RocketmqFactory;
 
 /**
- * 
  * <Description> <br>
  * 
  * @author 大刘杰<br>
@@ -25,71 +24,70 @@ import com.hbasesoft.framework.message.rocketmq.factory.RocketmqFactory;
  */
 public class RocketmqMessagePublisher implements MessagePublisher {
 
-	private static final Logger log = new Logger(RocketmqMessagePublisher.class);
+    private static final Logger log = new Logger(RocketmqMessagePublisher.class);
 
-	@Override
-	public String getName() {
-		return RocketmqFactory.ROCKET_MQ_NAME;
-	}
+    @Override
+    public String getName() {
+        return RocketmqFactory.ROCKET_MQ_NAME;
+    }
 
-	@Override
-	public void publish(String channel, byte[] data) {
-		// 默认使用普通消费
-		publish(channel, data, RocketmqFactory.ROCKET_MQ_DEFAULT_PUBLISH_TYPE, channel, 0L);
-	}
+    @Override
+    public void publish(String channel, byte[] data) {
+        // 默认使用普通消费
+        publish(channel, data, RocketmqFactory.ROCKET_MQ_DEFAULT_PUBLISH_TYPE, 0L);
+    }
 
-	public void publish(String channel, byte[] data, Long delayTime) {
-		// 默认使用普通消费
-		publish(channel, data, RocketmqFactory.ROCKET_MQ_DEFAULT_PUBLISH_TYPE, channel, delayTime);
-	}
+    public void publish(String channel, byte[] data, Long delayTime) {
+        // 默认使用普通消费
+        publish(channel, data, RocketmqFactory.ROCKET_MQ_DEFAULT_PUBLISH_TYPE, delayTime);
+    }
 
-	/**
-	 * 
-	 * publish:. <br/>
-	 * 
-	 * @author 大刘杰
-	 * @param channel
-	 * @param data
-	 * @param produceModel
-	 * @param producerGroup
-	 * @since JDK 1.8
-	 * @produceModel: RocketmqAutoConfiguration.ROCKET_MQ_DEFAULT_PUBLISH_TYPE
-	 *                RocketmqAutoConfiguration.ROCKET_MQ_DEFAULT_PUBLISH_TYPE
-	 *                RocketmqAutoConfiguration.ROCKET_MQ_DEFAULT_PUBLISH_TYPE
-	 */
-	public void publish(String channel, byte[] data, String produceModel, String producerGroup, Long delayTime) {
+    /**
+     * publish:. <br/>
+     * 
+     * @author 大刘杰
+     * @param channel
+     * @param data
+     * @param produceModel
+     * @param producerGroup
+     * @since JDK 1.8
+     * @produceModel: RocketmqAutoConfiguration.ROCKET_MQ_DEFAULT_PUBLISH_TYPE
+     *                RocketmqAutoConfiguration.ROCKET_MQ_DEFAULT_PUBLISH_TYPE
+     *                RocketmqAutoConfiguration.ROCKET_MQ_DEFAULT_PUBLISH_TYPE
+     */
+    public void publish(String channel, byte[] data, String produceModel, Long delayTime) {
 
-		Assert.notEmpty(producerGroup, ErrorCodeDef.MESSAGE_MODEL_EMPTY_P_GROUP_NAME);
+        DefaultMQProducer defaultMQProducer = RocketmqFactory
+            .getDefaultProducer(PropertyHolder.getProperty("message.rocketmq.groupName", "hbasesoft"));
 
-		DefaultMQProducer defaultMQProducer = RocketmqFactory.getDefaultProducer(producerGroup);
+        // Create a message instance, specifying topic, tag and message body.
+        Message msg = new Message(channel, GlobalConstants.BLANK, data);
 
-		// Create a message instance, specifying topic, tag and message body.
-		Message msg = new Message(channel, GlobalConstants.BLANK, data);
+        // Set delay level
+        if (delayTime > 0L) {
+            msg.setDelayTimeLevel(RocketmqFactory.calculationLevel(delayTime));
+        }
 
-		// Set delay level
-		if (delayTime > 0L) {
-			msg.setDelayTimeLevel(RocketmqFactory.calculationLevel(delayTime));
-		}
+        try {
+            switch (produceModel) {
+                case RocketmqFactory.ROCKET_MQ_PUBLISH_TYPE_ORDERLY:
+                    // 顺序消费
+                    // defaultMQProducer.send(msg, mq)
+                    break;
+                case RocketmqFactory.ROCKET_MQ_PUBLISH_TYPE_TRANSACTION:
+                    // 事务消费
+                    // transactionMQProducer.sendMessageInTransaction(msg, tranExecuter, arg);
+                    break;
+                default:
+                    // 普通消费
+                    SendResult send = defaultMQProducer.send(msg);
+                    log.info("发送结果 " + send.toString());
+                    break;
+            }
+        }
+        catch (Exception e) {
+            throw new UtilException(ErrorCodeDef.MESSAGE_MODEL_P_SEND_ERROR, e);
+        }
 
-		try {
-			switch (produceModel) {
-			case RocketmqFactory.ROCKET_MQ_PUBLISH_TYPE_ORDERLY:
-				// 顺序消费
-				// defaultMQProducer.send(msg, mq)
-				break;
-			case RocketmqFactory.ROCKET_MQ_PUBLISH_TYPE_TRANSACTION:
-				// 事务消费
-				// transactionMQProducer.sendMessageInTransaction(msg, tranExecuter, arg);
-				break;
-			default:
-				// 普通消费
-				SendResult send = defaultMQProducer.send(msg);
-				log.info("发送结果 " + send.toString());
-				break;
-			}
-		} catch (Exception e) {
-			throw new UtilException(ErrorCodeDef.MESSAGE_MODEL_P_SEND_ERROR, e);
-		}
-
-	}
+    }
 }
--- framework-message/framework-message-rocketMQ/src/test/java/com/hbasesoft/framework/message/rocketmq/MessageServApplicationTest.java
+++ framework-message/framework-message-rocketMQ/src/test/java/com/hbasesoft/framework/message/rocketmq/MessageServApplicationTest.java
@@ -5,7 +5,6 @@
  ****************************************************************************************/
 package com.hbasesoft.framework.message.rocketmq;
 
-import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -16,8 +15,6 @@ import org.springframework.context.annotation.ImportResource;
 
 import com.hbasesoft.framework.common.Bootstrap;
 import com.hbasesoft.framework.message.core.event.EventData;
-import com.hbasesoft.framework.message.core.event.EventEmmiter;
-import com.hbasesoft.framework.message.rocketmq.RocketmqMessagePublisher;
 
 /**
  * <Description> <br>
@@ -29,40 +26,41 @@ import com.hbasesoft.framework.message.rocketmq.RocketmqMessagePublisher;
  * @since V1.0<br>
  * @see com.hbasesoft.vcc.sgp.bootstrap.plat <br>
  */
-@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
+@EnableAutoConfiguration(exclude = {
+    DataSourceAutoConfiguration.class
+})
 @ComponentScan(basePackages = "com.hbasesoft")
 @ImportResource("classpath*:META-INF/spring/*.xml")
 @Configuration
 public class MessageServApplicationTest {
-	/**
-	 * Description: <br>
-	 * 
-	 * @author 王伟<br>
-	 * @taskId <br>
-	 * @param args
-	 *            <br>
-	 */
-	// @Autowired
-	// private static RocketmqMessageSubscribe rms;
+    /**
+     * Description: <br>
+     * 
+     * @author 王伟<br>
+     * @taskId <br>
+     * @param args <br>
+     */
+    // @Autowired
+    // private static RocketmqMessageSubscribe rms;
 
-	// @Autowired
-	// private RocketmqMessagePublisher rocketmqMessagePublisher;
-	//
-	public static void main(String[] args) {
-		Bootstrap.before();
-		ConfigurableApplicationContext context = SpringApplication.run(MessageServApplicationTest.class, args);
-		Bootstrap.after(context);
+    // @Autowired
+    // private RocketmqMessagePublisher rocketmqMessagePublisher;
+    //
+    public static void main(String[] args) {
+        Bootstrap.before();
+        ConfigurableApplicationContext context = SpringApplication.run(MessageServApplicationTest.class, args);
+        Bootstrap.after(context);
 
-		EventData eventData = new EventData();
-		for (int i = 0; i < 10; i++) {
-			eventData.put("topic1", "Hello word" + i);
+        EventData eventData = new EventData();
+        for (int i = 0; i < 10; i++) {
+            eventData.put("topic1", "Hello word" + i);
 
-		}
-		
-		// RocketmqMessagePublisher rocketmqMessagePublisher =
-		// (RocketmqMessagePublisher)context.getBean(RocketmqMessagePublisher.class);
-		// for (int i = 0; i < 10; i++) {
-		// rocketmqMessagePublisher.publish("topic1", ("hello word" + i).getBytes());
-		// }
-	}
+        }
+
+        // RocketmqMessagePublisher rocketmqMessagePublisher =
+        // (RocketmqMessagePublisher)context.getBean(RocketmqMessagePublisher.class);
+        // for (int i = 0; i < 10; i++) {
+        // rocketmqMessagePublisher.publish("topic1", ("hello word" + i).getBytes());
+        // }
+    }
 }
