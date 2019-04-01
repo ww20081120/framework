e011f52ff0f9a60f8abc0374db4987f34dd1db0d
wangwei <ww20081120@139.com>
2019-04-01 16:40:47 +0800

    更新spring mvc的message convert

--- framework-common/src/main/java/com/hbasesoft/framework/common/config/MessageConfig.java
+++ framework-common/src/main/java/com/hbasesoft/framework/common/config/MessageConfig.java
@@ -5,11 +5,13 @@
  ****************************************************************************************/
 package com.hbasesoft.framework.common.config;
 
-import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
@@ -30,19 +32,28 @@ import com.hbasesoft.framework.common.annotation.NoTransLog;
  */
 @Configuration
 @NoTransLog
-public class MessageConfig {
+public class MessageConfig implements WebMvcConfigurer {
 
-    @Bean
-    public HttpMessageConverters fastJsonHttpMessageConverters() {
+    /**
+     * Description: <br>
+     * 
+     * @author 王伟<br>
+     * @taskId <br>
+     * @param converters <br>
+     */
+    @SuppressWarnings("deprecation")
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
         // 1、需要先定义一个 convert 转换消息对象；
         FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
 
         // 2、添加 fastJson 的配置信息，比如: 是否要格式化返回的Json数据；
         fastConverter.setFeatures(SerializerFeature.WriteDateUseDateFormat);
 
-        // 4、
-        HttpMessageConverter<?> converter = fastConverter;
-        return new HttpMessageConverters(new StringHttpMessageConverter(), converter);
+        converters.clear();
+        converters.add(new StringHttpMessageConverter());
+        converters.add(fastConverter);
+        WebMvcConfigurer.super.extendMessageConverters(converters);
     }
 
     @Bean
