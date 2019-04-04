af5e083ada48d4e9c3112d775a0050bce299368c
wangwei <ww20081120@139.com>
2019-04-04 11:33:51 +0800

    更新spring mvc的message convert

--- framework-common/src/main/java/com/hbasesoft/framework/common/config/MessageConfig.java
+++ framework-common/src/main/java/com/hbasesoft/framework/common/config/MessageConfig.java
@@ -5,9 +5,14 @@
  ****************************************************************************************/
 package com.hbasesoft.framework.common.config;
 
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -28,18 +33,18 @@ import com.hbasesoft.framework.common.annotation.NoTransLog;
 public class MessageConfig {
 
     // Spring MVC 项目请实现 org.springframework.web.servlet.config.annotation.WebMvcConfigurer
-    // @Bean
-    // public HttpMessageConverters fastJsonHttpMessageConverters() {
-    // // 1、需要先定义一个 convert 转换消息对象；
-    // FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
-    //
-    // // 2、添加 fastJson 的配置信息，比如: 是否要格式化返回的Json数据；
-    // fastConverter.setFeatures(SerializerFeature.WriteDateUseDateFormat);
-    //
-    // // 4、
-    // HttpMessageConverter<?> converter = fastConverter;
-    // return new HttpMessageConverters(new StringHttpMessageConverter(), converter);
-    // }
+    @Bean
+    public HttpMessageConverters fastJsonHttpMessageConverters() {
+        // 1、需要先定义一个 convert 转换消息对象；
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+
+        // 2、添加 fastJson 的配置信息，比如: 是否要格式化返回的Json数据；
+        fastConverter.setFeatures(SerializerFeature.WriteDateUseDateFormat);
+
+        // 4、
+        HttpMessageConverter<?> converter = fastConverter;
+        return new HttpMessageConverters(new StringHttpMessageConverter(), converter);
+    }
 
     @Bean
     public ObjectMapper serializingObjectMapper() {
