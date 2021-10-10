b90449eec811f741efbdb4a96ebecd517fd5639a
wangwei <ww20081120@126.com>
2021-10-11 05:21:02 +0800

    修改事件的注册信息

--- framework-common/src/main/java/com/hbasesoft/framework/common/utils/xml/XmlBeanUtil.java
+++ framework-common/src/main/java/com/hbasesoft/framework/common/utils/xml/XmlBeanUtil.java
@@ -71,7 +71,7 @@ public class XmlBeanUtil {
 
             marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true); // 格式化输出
             marshaller.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");// 编码格式,默认为utf-8
-            marshaller.setProperty(Marshaller.JAXB_FRAGMENT, true);// 是否省略xml头信息
+            //marshaller.setProperty(Marshaller.JAXB_FRAGMENT, true);// 是否省略xml头信息
             StringWriter writer = new StringWriter();
             marshaller.marshal(object, writer);
             String xml = writer.toString();
