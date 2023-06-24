7621a7f783d7db54127cb7aa41c6d0dc5720db32
wangwei <ww20081120@126.com>
2023-06-24 20:12:27 +0800

    合并3.7的代码

--- framework-job/framework-job-xxl/src/main/java/com/hbasesoft/framework/job/xxl/ProxyJob.java
+++ framework-job/framework-job-xxl/src/main/java/com/hbasesoft/framework/job/xxl/ProxyJob.java
@@ -5,10 +5,9 @@
  ****************************************************************************************/
 package com.hbasesoft.framework.job.xxl;
 
-import com.hbasesoft.framework.common.utils.CommonUtil;
 import com.hbasesoft.framework.job.core.JobContext;
 import com.hbasesoft.framework.job.core.SimpleJob;
-import com.xxl.job.core.biz.model.ReturnT;
+import com.xxl.job.core.context.XxlJobHelper;
 import com.xxl.job.core.handler.IJobHandler;
 
 /**
@@ -29,9 +28,6 @@ public class ProxyJob extends IJobHandler {
     /** */
     private final String jobName;
 
-    /** */
-    private final String id;
-
     /**
      * @Method ProxyJob
      * @param jobName
@@ -44,7 +40,6 @@ public class ProxyJob extends IJobHandler {
     public ProxyJob(final String jobName, final SimpleJob job) {
         this.job = job;
         this.jobName = jobName;
-        this.id = CommonUtil.getTransactionID();
     }
 
     /**
@@ -52,15 +47,13 @@ public class ProxyJob extends IJobHandler {
      * 
      * @author 王伟<br>
      * @taskId <br>
-     * @param arg0
-     * @return
      * @throws Exception <br>
      */
     @Override
-    public ReturnT<String> execute(final String arg0) throws Exception {
-        JobContext jobContext = new JobContext(jobName, id, 1, arg0, 1, null);
+    public void execute() throws Exception {
+        JobContext jobContext = new JobContext(jobName, String.valueOf(XxlJobHelper.getJobId()),
+            XxlJobHelper.getShardTotal(), XxlJobHelper.getJobParam(), XxlJobHelper.getShardIndex(), null);
         job.execute(jobContext);
-        return ReturnT.SUCCESS;
     }
 
 }
