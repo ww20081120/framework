3bc40e0b2633ec6b3fb27a22be0e085405a424ed
wangwei <ww20081120@126.com>
2024-01-12 12:41:50 +0800

    fix: 让xxljob使用spring的方式取配置项

--- framework-job/framework-job-xxl/src/main/java/com/hbasesoft/framework/job/xxl/XxlJobConfig.java
+++ framework-job/framework-job-xxl/src/main/java/com/hbasesoft/framework/job/xxl/XxlJobConfig.java
@@ -6,6 +6,7 @@
 package com.hbasesoft.framework.job.xxl;
 
 import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -31,6 +32,42 @@ public class XxlJobConfig {
     /** */
     private static final int NUM9999 = 9999;
 
+    /** 是否启用job */
+    @Value("${job.enable}")
+    private boolean enable = true;
+
+    /** 调度中心部署根地址 [选填]：如调度中心集群部署存在多个地址则用逗号分隔。执行器将会使用该地址进行"执行器心跳注册"和"任务结果回调"；为空则关闭自动注册 */
+    @Value("${job.xxl.admin.addresses}")
+    private String adminAddresses = "http://127.0.0.1:8080/xxl-job-admin";
+
+    /** 执行器通讯TOKEN [选填]：非空时启用； */
+    @Value("${job.xxl.accessToken}")
+    private String accessToken;
+
+    /** 执行器AppName [选填]：执行器心跳注册分组依据；为空则关闭自动注册 */
+    @Value("${job.xxl.executor.appname}")
+    private String appname = "xxl-job-executor-sample";
+
+    /** 执行器注册 [选填]：优先使用该配置作为注册地址，为空时使用内嵌服务 ”IP:PORT“ 作为注册地址。从而更灵活的支持容器类型执行器动态IP和动态映射端口问题 */
+    @Value("${job.xxl.executor.address}")
+    private String address;
+
+    /** 执行器IP [选填]：默认为空表示自动获取IP，多网卡时可手动设置指定IP，该IP不会绑定Host仅作为通讯实用；地址信息用于 "执行器注册" 和 "调度中心请求并触发任务"； */
+    @Value("${job.xxl.executor.ip}")
+    private String ip;
+
+    /** 执行器端口号 [选填]：小于等于0则自动获取；默认端口为9999，单机部署多个执行器时，注意要配置不同执行器端口； */
+    @Value("${job.xxl.executor.port}")
+    private int port = NUM9999;
+
+    /** 执行器运行日志文件存储磁盘路径 [选填] ：需要对该路径拥有读写权限；为空则使用默认路径； */
+    @Value("${job.xxl.executor.logpath}")
+    private String logPath;
+
+    /** 执行器日志文件保存天数 [选填] ： 过期日志自动清理, 限制值大于等于3时生效; 否则, 如-1, 关闭自动清理功能； */
+    @Value("${job.xxl.executor.logretentiondays}")
+    private int logRetentionDays = NUM30;
+
     /**
      * @Method xxlJobExecutor
      * @param
@@ -38,44 +75,42 @@ public class XxlJobConfig {
      * @Author 李煜龙
      * @Description TODD
      * @Date 2023/1/29 16:13
-    */
+     */
     @Bean
     public XxlJobSpringExecutor xxlJobExecutor() {
 
         // 未开启Job则不进行扫描
-        if (!PropertyHolder.getBooleanProperty("job.enable", true)) {
+        if (!enable) {
             return null;
         }
 
         XxlJobSpringExecutor xxlJobSpringExecutor = new XxlJobSpringExecutor();
-        // 调度中心部署根地址 [选填]：如调度中心集群部署存在多个地址则用逗号分隔。执行器将会使用该地址进行"执行器心跳注册"和"任务结果回调"；为空则关闭自动注册
-        xxlJobSpringExecutor.setAdminAddresses(
-            PropertyHolder.getProperty("job.xxl.admin.addresses", "http://127.0.0.1:8080/xxl-job-admin"));
+        xxlJobSpringExecutor.setAdminAddresses(adminAddresses);
 
         // 执行器通讯TOKEN [选填]：非空时启用；
-        String accessToken = PropertyHolder.getProperty("job.xxl.accessToken");
         if (StringUtils.isNotEmpty(accessToken)) {
             xxlJobSpringExecutor.setAccessToken(accessToken);
         }
 
         // 执行器AppName [选填]：执行器心跳注册分组依据；为空则关闭自动注册
-        xxlJobSpringExecutor
-            .setAppname(PropertyHolder.getProperty("job.xxl.executor.appname", "xxl-job-executor-sample"));
-        String ip = PropertyHolder.getProperty("job.xxl.executor.ip");
+        xxlJobSpringExecutor.setAppname(appname);
+
         if (StringUtils.isNotEmpty(ip)) {
             xxlJobSpringExecutor.setIp(ip);
         }
+        else {
+            xxlJobSpringExecutor.setAddress(address);
+        }
         // 执行器端口号 [选填]：小于等于0则自动获取；默认端口为9999，单机部署多个执行器时，注意要配置不同执行器端口；
-        xxlJobSpringExecutor.setPort(PropertyHolder.getIntProperty("job.xxl.executor.port", NUM9999));
+        xxlJobSpringExecutor.setPort(port);
 
         // 执行器运行日志文件存储磁盘路径 [选填] ：需要对该路径拥有读写权限；为空则使用默认路径；
-        xxlJobSpringExecutor.setLogPath(PropertyHolder.getProperty("job.xxl.executor.logpath",
-            new StringBuilder().append(System.getProperty("user.home")).append("/log/hbasesoft/")
-                .append(PropertyHolder.getProjectName()).toString()));
+        xxlJobSpringExecutor.setLogPath(StringUtils.isNotEmpty(logPath) ? logPath
+            : new StringBuilder().append(System.getProperty("user.home")).append("/log/hbasesoft/")
+                .append(PropertyHolder.getProjectName()).toString());
 
         // 执行器日志文件保存天数 [选填] ： 过期日志自动清理, 限制值大于等于3时生效; 否则, 如-1, 关闭自动清理功能；
-        xxlJobSpringExecutor
-            .setLogRetentionDays(PropertyHolder.getIntProperty("job.xxl.executor.logretentiondays", NUM30));
+        xxlJobSpringExecutor.setLogRetentionDays(logRetentionDays);
         return xxlJobSpringExecutor;
 
     }
