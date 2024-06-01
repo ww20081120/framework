fc52c7617dd43517b9693e2f8d5254ebf06b0688
wangwei <ww20081120@126.com>
2024-06-01 23:00:19 +0800

    fix: 增加framework-tracing的发送方式

---
+++ framework-tracing/framework-tracing-file/src/main/java/com/hbasesoft/framework/tracing/file/OpenTelemetryConfig.java
@@ -0,0 +1,52 @@
+/**************************************************************************************** 
+ Copyright © 2003-2012 hbasesoft Corporation. All rights reserved. Reproduction or       <br>
+ transmission in whole or in part, in any form or by any means, electronic, mechanical <br>
+ or otherwise, is prohibited without the prior written consent of the copyright owner. <br>
+ ****************************************************************************************/
+package com.hbasesoft.framework.tracing.file;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.hbasesoft.framework.common.utils.PropertyHolder;
+
+import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.exporter.logging.LoggingSpanExporter;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.resources.Resource;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor;
+import io.opentelemetry.semconv.ResourceAttributes;
+
+/**
+ * <Description> <br>
+ * 
+ * @author ww200<br>
+ * @version 1.0<br>
+ * @taskId <br>
+ * @CreateDate 2024年6月1日 <br>
+ * @since V1.0<br>
+ * @see com.hbasesoft.framework.tracing.file <br>
+ */
+@Configuration
+public class OpenTelemetryConfig {
+    /**
+     * Description: <br>
+     * 
+     * @author 王伟<br>
+     * @taskId <br>
+     * @return <br>
+     */
+    @Bean
+    public OpenTelemetrySdk openTelemetrySdk() {
+        Resource resource = Resource.getDefault()
+            .merge(Resource.create(Attributes.of(ResourceAttributes.SERVICE_NAME, PropertyHolder.getProjectName())));
+
+        LoggingSpanExporter exporter = LoggingSpanExporter.create();
+
+        SdkTracerProvider sdkTracerProvider = SdkTracerProvider.builder()
+            .addSpanProcessor(SimpleSpanProcessor.create(exporter)).setResource(resource).build();
+
+        return OpenTelemetrySdk.builder().setTracerProvider(sdkTracerProvider).buildAndRegisterGlobal();
+    }
+}
