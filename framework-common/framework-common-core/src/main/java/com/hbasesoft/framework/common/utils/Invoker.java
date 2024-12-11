565b89c53e260099537e3b2cf834971b0354a7d4
wangwei <13601453189>
2024-12-11 13:34:34 +0800

    fix: 增加网关模块

---
+++ framework-common/framework-common-core/src/main/java/com/hbasesoft/framework/common/utils/Invoker.java
@@ -0,0 +1,29 @@
+/**************************************************************************************** 
+ Copyright © 2003-2012 hbasesoft Corporation. All rights reserved. Reproduction or       <br>
+ transmission in whole or in part, in any form or by any means, electronic, mechanical <br>
+ or otherwise, is prohibited without the prior written consent of the copyright owner. <br>
+ ****************************************************************************************/
+package com.hbasesoft.framework.common.utils;
+
+/**
+ * <Description> <br>
+ * 
+ * @author 王伟<br>
+ * @version 1.0<br>
+ * @taskId <br>
+ * @CreateDate 2024年12月11日 <br>
+ * @since V1.0<br>
+ * @see com.hbasesoft.framework.common.utils <br>
+ */
+@FunctionalInterface
+public interface Invoker<T> {
+
+    /**
+     * Description: 执行 <br>
+     * 
+     * @author 王伟<br>
+     * @taskId <br>
+     * @return <br>
+     */
+    T invoke() throws Throwable;
+}
