3f5cd65cdf2feb1acba8b1b31efdb3d631957293
wangwei <ww20081120@139.com>
2025-08-23 17:57:44 +0800

    feat: 添加AI代理相关功能和配置

--- framework-db/framework-db-core/src/main/java/com/hbasesoft/framework/db/core/criteria/lambda/LambdaSett.java
+++ framework-db/framework-db-core/src/main/java/com/hbasesoft/framework/db/core/criteria/lambda/LambdaSett.java
@@ -18,61 +18,58 @@ import lombok.Data;
 @Data
 public class LambdaSett {
 
-    /**
-     * 字符串
-     */
-    public static final int STR = 0;
+	/**
+	 * 字符串
+	 */
+	public static final int STR = 0;
 
-    /**
-     * 日期
-     */
-    public static final int DATE = 1;
+	/**
+	 * 日期
+	 */
+	public static final int DATE = 1;
 
-    /**
-     * 数字
-     */
-    public static final int NUMBER = 2;
+	/**
+	 * 数字
+	 */
+	public static final int NUMBER = 2;
 
-    /**
-     * 字段
-     */
-    private Field field;
+	/**
+	 * 字段
+	 */
+	private Field field;
 
-    /**
-     * 字段类型
-     */
-    private int fieldType;
+	/**
+	 * 字段类型
+	 */
+	private int fieldType;
 
-    /**
-     * 字段名
-     */
-    private String filedName;
+	/**
+	 * 字段名
+	 */
+	private String filedName;
 
-    /** implClass */
-    private Class<?> implClass;
+	/** implClass */
+	private Class<?> implClass;
 
-    /**
-     * @param lambda
-     * @param filedName
-     */
-    public LambdaSett(final SerializedLambda lambda, final String filedName) {
-        this.implClass = lambda.getImplClass();
-        field = ClassUtil.getDeclaredField(lambda.getImplClass(), filedName);
-        this.filedName = filedName;
-        Class<?> type = field.getType();
-        if (type == String.class) {
-            fieldType = STR;
-        }
-        else if (type == Number.class || Number.class.isAssignableFrom(type)) {
-            fieldType = NUMBER;
-        }
-        else if (type == Date.class || Date.class.isAssignableFrom(type)) {
-            fieldType = DATE;
-        }
-        else {
-            throw new DaoException(ErrorCodeDef.PARAM_ERROR,
-                filedName + " 类型错误， Wrapper<T> 只能用于 number string date, 注意：数字必须要用包装类型 ");
-        }
-    }
+	/**
+	 * @param lambda
+	 * @param filedName
+	 */
+	public LambdaSett(final SerializedLambda lambda, final String filedName) {
+		this.implClass = lambda.getImplClass();
+		field = ClassUtil.getDeclaredField(lambda.getImplClass(), filedName);
+		this.filedName = filedName;
+		Class<?> type = field.getType();
+		if (type == String.class) {
+			fieldType = STR;
+		} else if (type == Number.class || Number.class.isAssignableFrom(type)) {
+			fieldType = NUMBER;
+		} else if (type == Date.class || Date.class.isAssignableFrom(type)) {
+			fieldType = DATE;
+		} else {
+			throw new DaoException(ErrorCodeDef.PARAM_ERROR, lambda.getImplClass().getName() + "." + filedName
+					+ " 类型错误， Wrapper<T> 只能用于 number string date, 注意：数字必须要用包装类型 ");
+		}
+	}
 
 }
